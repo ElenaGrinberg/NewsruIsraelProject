4ae0e205b9d98ef6001542a2b0ee0cbedf895e97
ElenaGrinberg <elena.grinberg@gmail.com>
2020-10-23 20:13:09 +0300

    Test for news text was added to the site

---
+++ src/test/java/AddNewTextToNews.java
@@ -0,0 +1,14 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import utills.UseCaseBase;
+
+public class AddNewTextToNews extends UseCaseBase {
+    public static final Logger logger = LogManager.getLogger(AddNewTextToNews.class);
+
+    @Test
+    public void NewTextIsExist(){
+
+
+    }
+}
